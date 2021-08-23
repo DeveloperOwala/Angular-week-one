7fbec73d5262de2d6d8e2467a576d3d967a6318c
DeveloperOwala <otienoerick46@gmail.com>
2021-08-24 01:43:55 +0300

    adding tuggle  button

--- quote/src/app/quote/quote.component.ts
+++ quote/src/app/quote/quote.component.ts
@@ -43,12 +43,12 @@ export class QuoteComponent implements OnInit {
 //     }
 //   }
 
-//   addNewQuote(quote: Quotes) {
-//     let quoteLength = this.quotes.length;
-//     quote.id = quoteLength + 1;
-//     quote.completeDate = new Date(quote.completeDate)
-//     this.quotes.push(quote)
-//   }
+  addNewQuote(quote: Quotes) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
 
   constructor() { }
 
